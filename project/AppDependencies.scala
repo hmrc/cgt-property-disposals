9b419adcba332eff5a8075f55c2ef61ad7c08fed
Andrew Chung <26929776+AndyHWChung@users.noreply.github.com>
2019-07-04 11:57:06 +0100

    CPDP-233: Upgrade to play 2.6 (#2)

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -7,16 +7,16 @@ object AppDependencies {
 
   val compile = Seq(
 
-    "uk.gov.hmrc"             %% "simple-reactivemongo"     % "7.20.0-play-25",
-    "uk.gov.hmrc"             %% "bootstrap-play-25"        % "4.13.0"
+    "uk.gov.hmrc"             %% "simple-reactivemongo"     % "7.20.0-play-26",
+    "uk.gov.hmrc"             %% "bootstrap-play-26"        % "0.41.0"
   )
 
   val test = Seq(
     "org.scalatest"           %% "scalatest"                % "3.0.4"                 % "test",
     "com.typesafe.play"       %% "play-test"                % current                 % "test",
     "org.pegdown"             %  "pegdown"                  % "1.6.0"                 % "test, it",
-    "uk.gov.hmrc"             %% "service-integration-test" % "0.2.0"                 % "test, it",
-    "org.scalatestplus.play"  %% "scalatestplus-play"       % "2.0.0"                 % "test, it"
+    "uk.gov.hmrc"             %% "service-integration-test" % "0.9.0-play-26"         % "test, it",
+    "org.scalatestplus.play"  %% "scalatestplus-play"       % "3.1.2"                 % "test, it"
   )
 
 }
