55e104949bfcef248e96908bd853ff828c99664e
Andrew Chung <26929776+AndyHWChung@users.noreply.github.com>
2020-01-06 15:53:36 +0000

    upgraded to simple-reactivemongo 7.22.0 (#67)

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -4,7 +4,7 @@ import sbt._
 object AppDependencies {
 
   val compile = Seq(
-    "uk.gov.hmrc"       %% "simple-reactivemongo"     % "7.21.0-play-26",
+    "uk.gov.hmrc"       %% "simple-reactivemongo"     % "7.22.0-play-26",
     "uk.gov.hmrc"       %% "bootstrap-play-26"        % "1.3.0",
     "org.typelevel"     %% "cats-core"                % "1.6.1",
     "org.julienrf"      %% "play-json-derived-codecs" % "3.3",
