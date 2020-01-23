867dd5d4ca03796970c8a802bedeeb3e9339d40f
Mikail Khan <co.category@gmail.com>
2020-01-23 16:31:40 +0000

    upgrade cats and codec lib

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -6,8 +6,8 @@ object AppDependencies {
   val compile = Seq(
     "uk.gov.hmrc"       %% "simple-reactivemongo"     % "7.22.0-play-26",
     "uk.gov.hmrc"       %% "bootstrap-play-26"        % "1.3.0",
-    "org.typelevel"     %% "cats-core"                % "1.6.1",
-    "org.julienrf"      %% "play-json-derived-codecs" % "6.0.0",
+    "org.typelevel"     %% "cats-core"                % "2.1.0",
+    "org.julienrf"      %% "play-json-derived-codecs" % "7.0.0",
     "com.github.kxbmap" %% "configs"                  % "0.4.4"
   )
 
