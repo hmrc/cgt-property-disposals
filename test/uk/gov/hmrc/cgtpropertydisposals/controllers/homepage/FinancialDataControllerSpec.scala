79af33bbe280683b76acccd45695c1604c2070b7
Rambabu Posa <rambabu.posa@digital.hmrc.gov.uk>
2020-02-27 11:24:34 +0000

    unit tests

--- test/uk/gov/hmrc/cgtpropertydisposals/controllers/homepage/FinancialDataControllerSpec.scala
+++ test/uk/gov/hmrc/cgtpropertydisposals/controllers/homepage/FinancialDataControllerSpec.scala
@@ -16,14 +16,13 @@
 
 package uk.gov.hmrc.cgtpropertydisposals.controllers.homepage
 
-import java.time.LocalDateTime
+import java.time.{LocalDate, LocalDateTime}
 
 import akka.stream.Materializer
 import cats.data.EitherT
 import cats.instances.future._
-import play.api.libs.json.JsValue
-import play.api.mvc.Headers
-import play.api.mvc.Headers
+import play.api.libs.json.{JsValue, Json}
+import play.api.mvc.{Headers, Result}
 import play.api.test.Helpers._
 import play.api.test.Helpers.CONTENT_TYPE
 import uk.gov.hmrc.cgtpropertydisposals.Fake
@@ -37,6 +36,7 @@ import uk.gov.hmrc.cgtpropertydisposals.service.onboarding.AuditService
 import uk.gov.hmrc.http.HeaderCarrier
 import play.api.test.{FakeRequest, Helpers}
 import uk.gov.hmrc.cgtpropertydisposals.models.Generators._
+import uk.gov.hmrc.cgtpropertydisposals.models.ids.CgtReference
 
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.Future
@@ -50,10 +50,10 @@ class FinancialDataControllerSpec extends ControllerSpec {
 
   implicit val headerCarrier = HeaderCarrier()
 
-  def mockGetFinancialDataService(financialData: FinancialDataRequest)(response: Either[Error, FinancialDataResponse]) =
+  def mockGetFinancialData(financialData: FinancialDataRequest)(response: Either[Error, FinancialDataResponse]) =
     (financialDataService
-      .getFinancialData(_: FinancialDataRequest))
-      .expects(financialData)
+      .getFinancialData(_: FinancialDataRequest)(_: HeaderCarrier))
+      .expects(financialData, *)
       .returning(EitherT.fromEither[Future](response))
 
   implicit lazy val mat: Materializer = fakeApplication.materializer
@@ -74,9 +74,67 @@ class FinancialDataControllerSpec extends ControllerSpec {
     cc                   = Helpers.stubControllerComponents()
   )
 
+  def getFinancialDataRequest(r: FinancialDataRequest) =
+    FinancialDataRequest(
+      idType     = "ZCGT",
+      idNumber   = r.idNumber,
+      regimeType = "CGT",
+      fromDate   = LocalDate.now(),
+      toDate     = LocalDate.now()
+    )
+
+  val request2 = getFinancialDataRequest(financialDataRequest)
+
   "FinancialDataController" when {
 
-    "handling requests to get financial data" must {}
+    "handling requests to get financial data" must {
+
+      def performAction(cgtReference: CgtReference, fromDate: String, toDate: String): Future[Result] =
+        controller.getFinancialData(cgtReference.value, fromDate, toDate)(request)
+
+      val cgtReference       = sample[CgtReference]
+      val (fromDate, toDate) = LocalDate.of(2020, 1, 31) -> LocalDate.of(2020, 11, 2)
+
+      "return an error" when {
+
+        "the fromDate cannot be parsed" in {
+          val result = performAction(cgtReference, "20203101", "2020-11-02")
+          status(result) shouldBe BAD_REQUEST
+        }
+
+        "the toDate cannot be parsed" in {
+          val result = performAction(cgtReference, "2020-01-31", "20203101")
+          status(result) shouldBe BAD_REQUEST
+        }
+
+        "neither the fromDate or toDate can be parsed" in {
+          val result = performAction(cgtReference, "20000101", "20203101")
+          status(result) shouldBe BAD_REQUEST
+        }
+
+        "there is a problem getting the financial data" in {
+
+          mockGetFinancialData(request2)(Left(Error("")))
+
+          val result = performAction(CgtReference(request2.idNumber), "2020-01-31", "2020-11-02")
+          status(result) shouldBe INTERNAL_SERVER_ERROR
+        }
+
+      }
+
+      "return financial data" when {
+
+        "financial data is successfully retrieved" in {
+          val response = sample[FinancialDataResponse]
+          mockGetFinancialData(request2)(Right(response))
+
+          val result = performAction(CgtReference(request2.idNumber), "2020-01-31", "2020-11-02")
+          status(result)        shouldBe 200
+          contentAsJson(result) shouldBe Json.toJson(response)
+        }
+
+      }
+    }
 
   }
 
--- test/uk/gov/hmrc/cgtpropertydisposals/models/Generators.scala
+++ test/uk/gov/hmrc/cgtpropertydisposals/models/Generators.scala
@@ -37,7 +37,7 @@ import uk.gov.hmrc.cgtpropertydisposals.models.returns.SingleDisposalTriageAnswe
 import scala.reflect.{ClassTag, classTag}
 import uk.gov.hmrc.cgtpropertydisposals.models.address.{Address, Country, Postcode}
 import uk.gov.hmrc.cgtpropertydisposals.models.address.Address.{NonUkAddress, UkAddress}
-import uk.gov.hmrc.cgtpropertydisposals.models.des.homepage.FinancialDataRequest
+import uk.gov.hmrc.cgtpropertydisposals.models.des.homepage.{FinancialDataRequest, FinancialDataResponse}
 import uk.gov.hmrc.cgtpropertydisposals.models.returns.ExemptionAndLossesAnswers.CompleteExemptionAndLossesAnswers
 
 object Generators
@@ -50,7 +50,7 @@ object Generators
     with DraftReturnGen
     with ReturnsGen
     with AddressGen
-    with FinancialDataRequestGen {
+    with FinancialDataGen {
 
   def sample[A: ClassTag](implicit gen: Gen[A]): A =
     gen.sample.getOrElse(sys.error(s"Could not generate instance of ${classTag[A].runtimeClass.getSimpleName}"))
@@ -196,8 +196,9 @@ trait AddressLowerPriorityGen { this: GenUtils =>
 
 }
 
-trait FinancialDataRequestGen { this: GenUtils =>
+trait FinancialDataGen { this: GenUtils =>
 
-  implicit val financialDataRequestGen: Gen[FinancialDataRequest] = gen[FinancialDataRequest]
+  implicit val financialDataRequestGen: Gen[FinancialDataRequest]   = gen[FinancialDataRequest]
+  implicit val financialDataResponseGen: Gen[FinancialDataResponse] = gen[FinancialDataResponse]
 
 }
