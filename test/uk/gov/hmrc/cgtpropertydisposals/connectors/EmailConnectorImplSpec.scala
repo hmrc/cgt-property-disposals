21d52a7ea4b712b022c34cee8903d5e9242df478
Andy <AndyHWChung@users.noreply.github.com>
2020-03-30 12:21:54 +0100

    cpdp-1257: only submissionid and name sent to email

--- test/uk/gov/hmrc/cgtpropertydisposals/connectors/EmailConnectorImplSpec.scala
+++ test/uk/gov/hmrc/cgtpropertydisposals/connectors/EmailConnectorImplSpec.scala
@@ -16,50 +16,22 @@
 
 package uk.gov.hmrc.cgtpropertydisposals.connectors
 
-import java.time.LocalDate
-
 import com.typesafe.config.ConfigFactory
 import org.scalamock.scalatest.MockFactory
 import org.scalatest.{Matchers, WordSpec}
-import play.api.i18n.{Lang, MessagesImpl, MessagesProvider}
 import play.api.libs.json.Json
-import play.api.test.Helpers
 import play.api.test.Helpers._
 import play.api.{Configuration, Mode}
 import uk.gov.hmrc.cgtpropertydisposals.models.Generators._
-import uk.gov.hmrc.cgtpropertydisposals.models.finance.{AmountInPence, MoneyUtils}
 import uk.gov.hmrc.cgtpropertydisposals.models.ids.CgtReference
 import uk.gov.hmrc.cgtpropertydisposals.models.onboarding.subscription.{SubscribedDetails, SubscriptionDetails}
 import uk.gov.hmrc.cgtpropertydisposals.models.returns.SubmitReturnResponse
-import uk.gov.hmrc.cgtpropertydisposals.models.returns.SubmitReturnResponse.ReturnCharge
 import uk.gov.hmrc.http.{HeaderCarrier, HttpResponse}
 import uk.gov.hmrc.play.bootstrap.config.{RunMode, ServicesConfig}
 
 import scala.concurrent.ExecutionContext.Implicits.global
 
 class EmailConnectorImplSpec extends WordSpec with Matchers with MockFactory with HttpSupport {
-  val messagesKeys = Map()
-
-  val monthNames: Map[String, Map[String, String]] = Map(
-    Lang.defaultLang.code -> Map(
-      "date.1"  -> "January",
-      "date.2"  -> "February",
-      "date.3"  -> "March",
-      "date.4"  -> "April",
-      "date.5"  -> "May",
-      "date.6"  -> "June",
-      "date.7"  -> "July",
-      "date.8"  -> "August",
-      "date.9"  -> "September",
-      "date.10" -> "October",
-      "date.11" -> "November",
-      "date.12" -> "December"
-    )
-  )
-
-  implicit val messagesApi                        = Helpers.stubMessagesApi(monthNames)
-  implicit val lang: Lang                         = Lang.apply("en-GB")
-  implicit val messagesProvider: MessagesProvider = MessagesImpl(lang, messagesApi)
 
   val (accountCreatedTemplateId, accountCreatedSignInUrl, returnSubmittedTemplateId) =
     ("template", "sign-in", "template-return-submitted")
@@ -93,7 +65,7 @@ class EmailConnectorImplSpec extends WordSpec with Matchers with MockFactory wit
   implicit val hc: HeaderCarrier = HeaderCarrier()
 
   val connector =
-    new EmailConnectorImpl(mockHttp, new ServicesConfig(config, new RunMode(config, Mode.Test)), messagesApi)
+    new EmailConnectorImpl(mockHttp, new ServicesConfig(config, new RunMode(config, Mode.Test)))
 
   "EmailConnectorImpl" when {
 
@@ -150,14 +122,10 @@ class EmailConnectorImplSpec extends WordSpec with Matchers with MockFactory wit
       }
     }
 
-    "it receives a request to send a return submitted confirmation email with SOME charge" must {
+    "it receives a request to send a return submitted confirmation email" must {
 
-      val taxDue               = AmountInPence(50)
-      val chargeRef            = "CHARGE_REF"
       val submissionId         = "submissionId"
-      val dueDate              = LocalDate.of(2020, 7, 1)
-      val charge               = ReturnCharge(chargeRef, taxDue, dueDate)
-      val submitReturnResponse = sample[SubmitReturnResponse].copy(formBundleId = submissionId, charge = Some(charge))
+      val submitReturnResponse = sample[SubmitReturnResponse].copy(formBundleId = submissionId)
       val subscribedDetails    = sample[SubscribedDetails]
       val expectedRequestBody = Json.parse(
         s"""{
@@ -165,10 +133,7 @@ class EmailConnectorImplSpec extends WordSpec with Matchers with MockFactory wit
            |  "templateId": "$returnSubmittedTemplateId",
            |  "parameters": {
            |    "name": "${subscribedDetails.contactName.value}",
-           |    "submissionId": "$submissionId",
-           |    "taxDue": "${MoneyUtils.formatAmountOfMoneyWithPoundSign(taxDue.value)}",
-           |    "chargeRef": "$chargeRef",
-           |    "dueDate": "1 July 2020"
+           |    "submissionId": "$submissionId"
            |  },
            |  "force": false
            |}
@@ -209,59 +174,6 @@ class EmailConnectorImplSpec extends WordSpec with Matchers with MockFactory wit
         }
       }
     }
-    "it receives a request to send a return submitted confirmation email with NO charge" must {
 
-      val taxDue               = AmountInPence(0)
-      val submissionId         = "submissionId"
-      val submitReturnResponse = sample[SubmitReturnResponse].copy(formBundleId = submissionId, charge = None)
-      val subscribedDetails    = sample[SubscribedDetails]
-      val expectedRequestBody = Json.parse(
-        s"""{
-           |  "to": ["${subscribedDetails.emailAddress.value}"],
-           |  "templateId": "$returnSubmittedTemplateId",
-           |  "parameters": {
-           |    "name": "${subscribedDetails.contactName.value}",
-           |    "submissionId": "$submissionId",
-           |    "taxDue": "${MoneyUtils.formatAmountOfMoneyWithPoundSign(taxDue.value)}"
-           |  },
-           |  "force": false
-           |}
-           |""".stripMargin
-      )
-
-      "make a http put call and return a result" in {
-        List(
-          HttpResponse(204),
-          HttpResponse(401),
-          HttpResponse(400)
-        ).foreach { httpResponse =>
-          withClue(s"For http response [${httpResponse.toString}]") {
-
-            mockPost(
-              s"http://host:123/hmrc/email",
-              Map.empty,
-              expectedRequestBody
-            )(Some(httpResponse))
-
-            await(connector.sendReturnSubmitConfirmationEmail(submitReturnResponse, subscribedDetails).value) shouldBe Right(
-              httpResponse
-            )
-          }
-        }
-      }
-
-      "return an error" when {
-
-        "the call fails" in {
-          mockPost(
-            s"http://host:123/hmrc/email",
-            Map.empty,
-            expectedRequestBody
-          )(None)
-
-          await(connector.sendReturnSubmitConfirmationEmail(submitReturnResponse, subscribedDetails).value).isLeft shouldBe true
-        }
-      }
-    }
   }
 }
