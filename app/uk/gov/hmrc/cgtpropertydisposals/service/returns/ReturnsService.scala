ff3ef7244b58005dd98643f454a7ef314cd5b789
Andrew Chung <26929776+AndyHWChung@users.noreply.github.com>
2020-03-31 18:35:09 +0100

    cpdp-1257: fixed way of inspecting result from email service (#154)
    Co-authored-by: Andy <AndyHWChung@users.noreply.github.com>

--- app/uk/gov/hmrc/cgtpropertydisposals/service/returns/ReturnsService.scala
+++ app/uk/gov/hmrc/cgtpropertydisposals/service/returns/ReturnsService.scala
@@ -100,9 +100,14 @@ class DefaultReturnsService @Inject() (
     for {
       _                  <- auditReturnBeforeSubmit(returnRequest, desSubmitReturnRequest)
       returnHttpResponse <- submitReturnAndAudit(returnRequest, desSubmitReturnRequest)
-      desResponse        <- parseResponse(returnHttpResponse)
-      returnResponse     <- prepareSubmitReturnResponse(desResponse)
-      _                  <- sendEmailAndAudit(returnRequest, returnResponse)
+      desResponse <- EitherT.fromEither[Future](
+                      returnHttpResponse.parseJSON[DesSubmitReturnResponse]().leftMap(Error(_))
+                    )
+      returnResponse <- prepareSubmitReturnResponse(desResponse)
+      _ <- sendEmailAndAudit(returnRequest, returnResponse).leftFlatMap { e =>
+            logger.warn("Could not send return submission confirmation email or audit event", e)
+            EitherT.pure[Future, Error](())
+          }
     } yield returnResponse
   }
 
@@ -149,22 +154,11 @@ class DefaultReturnsService @Inject() (
   private def sendEmailAndAudit(
     returnRequest: SubmitReturnRequest,
     returnResponse: SubmitReturnResponse
-  )(implicit hc: HeaderCarrier, request: Request[_]): EitherT[Future, Error, SubmitReturnResponse] = {
-    val response = for {
+  )(implicit hc: HeaderCarrier, request: Request[_]): EitherT[Future, Error, Unit] =
+    for {
       _ <- sendReturnConfirmationEmail(returnRequest, returnResponse)
       _ <- auditSubscriptionConfirmationEmailSent(returnRequest, returnResponse)
-    } yield returnResponse
-    response.leftFlatMap(_ => EitherT.pure[Future, Error](returnResponse))
-  }
-
-  private def parseResponse(response: HttpResponse): EitherT[Future, Error, DesSubmitReturnResponse] =
-    EitherT.fromEither[Future](response.parseJSON[DesSubmitReturnResponse]() match {
-      case Right(response) => response.asRight[Error]
-      case Left(error) => {
-        logger.warn(s"Call to send return confirmation email audit event ${response.status}")
-        Error(error).asLeft[DesSubmitReturnResponse]
-      }
-    })
+    } yield ()
 
   private def sendReturnConfirmationEmail(
     returnRequest: SubmitReturnRequest,
@@ -176,18 +170,10 @@ class DefaultReturnsService @Inject() (
         returnRequest.subscribedDetails
       )
       .subflatMap { httpResponse =>
-        httpResponse match {
-          case HttpResponse(ACCEPTED, _, _, _) => Right(submitReturnResponse)
-          case _ => {
-            logger.warn(s"Call to send confirmation email came back with status ${httpResponse.status}")
-            Left(Error(s"Call to send confirmation email came back with status ${httpResponse.status}"))
-          }
-        }
-      }
-      .leftMap { e =>
-        metrics.submitReturnErrorCounter.inc()
-        logger.warn("Could not send confirmation email", e)
-        e
+        if (httpResponse.status === ACCEPTED)
+          Right(submitReturnResponse)
+        else
+          Left(Error(s"Call to send confirmation email came back with status ${httpResponse.status}"))
       }
 
   private def auditSubscriptionConfirmationEmailSent(
