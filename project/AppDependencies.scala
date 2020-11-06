0c01cf3a2e06bfaa29e0668a76fc8099950960a9
Andy <26929776+AndyHWChung@users.noreply.github.com>
2020-11-06 15:43:02 +0000

    CPDP-3336: update libraries

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -7,8 +7,8 @@ object AppDependencies {
 
   val compile = Seq(
     "uk.gov.hmrc"                             %% "simple-reactivemongo"      % "7.30.0-play-26",
-    "uk.gov.hmrc"                             %% "bootstrap-backend-play-26" % "2.24.0",
-    "uk.gov.hmrc"                             %% "work-item-repo"            % "7.6.0-play-26",
+    "uk.gov.hmrc"                             %% "bootstrap-backend-play-26" % "3.0.0",
+    "uk.gov.hmrc"                             %% "work-item-repo"            % "7.10.0-play-26",
     "org.typelevel"                           %% "cats-core"                 % "2.1.0",
     "org.julienrf"                            %% "play-json-derived-codecs"  % "7.0.0",
     "com.github.kxbmap"                       %% "configs"                   % "0.4.4",
@@ -22,7 +22,6 @@ object AppDependencies {
     "org.scalamock"              %% "scalamock"                  % "4.2.0"          % "test",
     "com.github.alexarchambault" %% "scalacheck-shapeless_1.14"  % "1.2.1"          % "test",
     "org.pegdown"                 % "pegdown"                    % "1.6.0"          % "test, it",
-    "uk.gov.hmrc"                %% "service-integration-test"   % "0.10.0-play-26" % "test, it",
     "org.scalatestplus.play"     %% "scalatestplus-play"         % "3.1.2"          % "test, it",
     "uk.gov.hmrc"                %% "reactivemongo-test"         % "4.21.0-play-26" % "test",
     "com.eclipsesource"          %% "play-json-schema-validator" % "0.9.5"          % "test",
