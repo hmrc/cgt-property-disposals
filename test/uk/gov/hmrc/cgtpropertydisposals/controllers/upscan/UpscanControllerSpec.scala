02ccc62cd056bd7be3fce021a0823c9a01de1be6
Rambabu Posa <rambabu.posa@digital.hmrc.gov.uk>
2021-03-02 14:49:23 +0000

    DLS-3997: Upscan changes from Map to UploadDetails case class.

--- test/uk/gov/hmrc/cgtpropertydisposals/controllers/upscan/UpscanControllerSpec.scala
+++ test/uk/gov/hmrc/cgtpropertydisposals/controllers/upscan/UpscanControllerSpec.scala
@@ -400,7 +400,8 @@ class UpscanControllerSpec extends ControllerSpec with ScalaCheckDrivenPropertyC
             ("uploadTimestamp", "2018-04-24T09:30:00Z"),
             ("checksum", "396f101dd52e8b2ace0dcf5ed09b1d1f030e608938510ce46e7a5c7a4e775100"),
             ("fileName", "test.pdf"),
-            ("fileMimeType", "application/pdf")
+            ("fileMimeType", "application/pdf"),
+            ("size", "12345")
           )
         )
 
@@ -414,7 +415,8 @@ class UpscanControllerSpec extends ControllerSpec with ScalaCheckDrivenPropertyC
             |        "uploadTimestamp": "2018-04-24T09:30:00Z",
             |        "checksum": "396f101dd52e8b2ace0dcf5ed09b1d1f030e608938510ce46e7a5c7a4e775100",
             |        "fileName": "test.pdf",
-            |        "fileMimeType": "application/pdf"
+            |        "fileMimeType": "application/pdf",
+            |        "size": 12345
             |    }
             |}
             |""".stripMargin
