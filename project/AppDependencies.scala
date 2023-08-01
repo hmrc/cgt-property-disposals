80be6814db424966d425a9bda7fe830f8066792c
johno1985 <1640705+johno1985@users.noreply.github.com>
2023-08-01 13:08:11 +0100

    DLS-6355: Removed unused test dependencies

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -1,4 +1,3 @@
-import play.core.PlayVersion.current
 import sbt.*
 
 object AppDependencies {
@@ -8,14 +7,6 @@ object AppDependencies {
   val mongoVersion = "0.68.0"
 
   private val bootstrapPlay28Version = "5.25.0"
-  private val mockitoCoreVersion     = "2.25.0"
-  private val pegdownVersion         = "1.6.0"
-  private val hmrcMongoVersion       = "0.68.0"
-  private val scalacheckRegexVersion = "0.1.1"
-  private val scalacheckVersion      = "1.14.0"
-  private val scalaTestPlusVersion   = "5.1.0"
-  private val scalaTestVersion       = "3.0.8"
-  private val wireMockVersion        = "2.21.0"
 
   val compile: Seq[ModuleID] = Seq(
     "uk.gov.hmrc"                             %% s"bootstrap-backend-$playVersion"         % bootstrapPlay28Version,
@@ -28,20 +19,11 @@ object AppDependencies {
   )
 
   def test(scope: String = "test"): Seq[ModuleID] = Seq(
-    "org.scalatest"              %% "scalatest"                  % "3.2.9"       % scope,
-    "com.typesafe.play"          %% "play-test"                  % current       % scope,
-    "org.scalamock"              %% "scalamock"                  % "5.1.0"       % scope,
-    "com.github.alexarchambault" %% "scalacheck-shapeless_1.14"  % "1.2.5"       % scope,
-    "org.scalatestplus.play"     %% "scalatestplus-play"         % "5.1.0"       % scope,
-    "org.scalatestplus"          %% "scalatestplus-scalacheck"   % "3.1.0.0-RC2" % scope,
-    "com.vladsch.flexmark"        % "flexmark-all"               % "0.35.10"     % scope,
-    "uk.gov.hmrc.mongo"          %% "hmrc-mongo-test-play-28"    % mongoVersion  % scope,
-    "com.eclipsesource"          %% "play-json-schema-validator" % "0.9.5"       % scope,
-    "com.typesafe.akka"          %% "akka-testkit"               % akkaVersion   % scope,
-    "com.typesafe.akka"          %% "akka-slf4j"                 % akkaVersion   % scope,
-    "com.typesafe.akka"          %% "akka-protobuf-v3"           % akkaVersion   % scope,
-    "com.typesafe.akka"          %% "akka-serialization-jackson" % akkaVersion   % scope,
-    "com.typesafe.akka"          %% "akka-stream"                % akkaVersion   % scope,
-    "com.typesafe.akka"          %% "akka-actor-typed"           % akkaVersion   % scope
+    "uk.gov.hmrc"                %% s"bootstrap-test-$playVersion" % bootstrapPlay28Version % scope,
+    "org.scalamock"              %% "scalamock"                    % "5.1.0"                % scope,
+    "com.github.alexarchambault" %% "scalacheck-shapeless_1.14"    % "1.2.5"                % scope,
+    "org.scalatestplus"          %% "scalatestplus-scalacheck"     % "3.1.0.0-RC2"          % scope,
+    "com.eclipsesource"          %% "play-json-schema-validator"   % "0.9.5"                % scope,
+    "com.typesafe.akka"          %% "akka-testkit"                 % akkaVersion            % scope
   )
 }
