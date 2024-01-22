80c06de72694959870793a81c7ec379606b4a4e1
johno1985 <1640705+johno1985@users.noreply.github.com>
2024-01-22 17:45:33 +0000

    DLS-9710: Update play-json-derived-codecs to version 10.1.0

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -11,7 +11,7 @@ object AppDependencies {
   val compile: Seq[ModuleID] = Seq(
     "uk.gov.hmrc"                             %% s"bootstrap-backend-$playVersion"         % bootstrapPlay28Version,
     "org.typelevel"                           %% "cats-core"                               % "2.9.0",
-    "org.julienrf"                            %% "play-json-derived-codecs"                % "10.0.2",
+    "org.julienrf"                            %% "play-json-derived-codecs"                % "10.1.0",
     "com.github.kxbmap"                       %% "configs"                                 % "0.6.1",
     "com.googlecode.owasp-java-html-sanitizer" % "owasp-java-html-sanitizer"               % "20191001.1",
     "uk.gov.hmrc.mongo"                       %% s"hmrc-mongo-work-item-repo-$playVersion" % mongoVersion
