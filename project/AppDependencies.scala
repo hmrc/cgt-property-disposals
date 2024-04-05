466134435c153d55f65af4a7b7ddfb4dec587832
johno1985 <1640705+johno1985@users.noreply.github.com>
2024-04-05 18:01:39 +0100

    DLS-10054: Updated cats-core to version 2.10.0

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -9,7 +9,7 @@ object AppDependencies {
 
   val compile: Seq[ModuleID] = Seq(
     "uk.gov.hmrc"                             %% s"bootstrap-backend-$playVersion"         % bootstrapVersion,
-    "org.typelevel"                           %% "cats-core"                               % "2.9.0",
+    "org.typelevel"                           %% "cats-core"                               % "2.10.0",
     "org.julienrf"                            %% "play-json-derived-codecs"                % "10.1.0",
     "com.github.pureconfig"                   %% "pureconfig"                              % "0.17.5",
     "com.googlecode.owasp-java-html-sanitizer" % "owasp-java-html-sanitizer"               % "20191001.1",
